45ec7ac84a356382212d3bff70b4d87dfa7a3e50
musaxasmammedov7 <musaxasmammedov7@icloud.com>
2026-05-01 21:06:07 +0400

    fix: restore backend connectivity and database parameters

--- start_backend.sh
+++ start_backend.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-# Запустить этой командой: bash start_backend.sh YOUR_SQL_USER YOUR_SQL_PASS
+# Запустить этой командой: bash start_backend.sh group2_sql BurgerApp2026!Secure
 
 SQL_USER=$1
 SQL_PASS=$2
+
+echo "Starting backend on VMSS instances..."
 
 az vmss run-command invoke \
   -g musa-project2-rg \
   -n burger-vmss-be-musa \
   --command-id RunShellScript \
-  --scripts "nohup sudo java -jar /home/azureuser/app.jar \
-    --spring.datasource.url='jdbc:sqlserver://burger-sqlserver.database.windows.net:1433;database=burgerbuilder-group22;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;' \
-    --spring.datasource.username='${SQL_USER}' \
-    --spring.datasource.password='${SQL_PASS}' \
-    --server.port=8080 \
-    > /home/azureuser/app.log 2>&1 < /dev/null &
-    sleep 15 && ss -tlnp && curl -sI http://localhost:8080/api/ingredients" \
-  --instance-id 1
+  --scripts "
+    sudo pkill -f 'java -jar' || true
+    sleep 2
+    nohup java -jar /home/azureuser/app.jar \
+      --spring.datasource.url='jdbc:sqlserver://burger-sqlserver.database.windows.net:1433;database=burgerbuilder-group22;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;' \
+      --spring.datasource.username='${SQL_USER}' \
+      --spring.datasource.password='${SQL_PASS}' \
+      --server.port=8080 \
+      > /home/azureuser/app.log 2>&1 < /dev/null &
+    sleep 5
+    ps aux | grep java
+  "
